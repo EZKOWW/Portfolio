bcd23772fd75b21ec2151dfd2c56cdc2098340d1
EZKOW <eloik.rousseau@gmail.com>
2022-11-15 13:41:36 -0500

    Added matrix background to header

---
+++ public/js/matrix.js
@@ -0,0 +1,41 @@
+let canvas = document.getElementById("matrix_bg");
+const ctx = canvas.getContext('2d');
+
+window.devicePixelRatio = 2;
+var size = 1200;
+
+var scale = window.devicePixelRatio;
+
+canvas.width = Math.floor(size * scale);
+canvas.height = Math.floor(size * scale);
+
+ctx.scale(scale, scale);
+
+const katakana = 'アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン';
+let fontSize = 12;
+let columns = canvas.width / fontSize;
+const rainDrops = [];
+
+for (let x = 0; x < columns; x++) {
+    rainDrops[x] = 1;
+}
+
+const draw = () => {
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    ctx.fillStyle = '#0F0';
+    ctx.font = fontSize + 'px monospace';
+
+    for (let i = 0; i < rainDrops.length; i++) {
+        const text = katakana.charAt(Math.floor(Math.random() * katakana.length));
+        ctx.fillText(text, i * fontSize, rainDrops[i] * fontSize);
+
+        if (rainDrops[i] * fontSize > canvas.height && Math.random() > 0.975) {
+            rainDrops[i] = 0;
+        }
+        rainDrops[i]++;
+    }
+};
+
+setInterval(draw, 25);
